97ef43e31b7776b72d31bbda440bf2cfe7a57d04
Giulia <giulialococo090203@gmail.com>
2026-06-13 16:40:02 +0200

    Allow Wrangler local origin for PDF parser

--- index.py
+++ index.py
@@ -27,6 +27,8 @@ ALLOWED_ORIGINS = [
     "http://127.0.0.1:4173",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:8788",
+    "http://127.0.0.1:8788",
 ]
 
 app.add_middleware(
